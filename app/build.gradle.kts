2e21a51de409e00ff006512e440589d01a9da33f
abhinaya baskkar <abhinaya.baskkar@gmail.com>
2026-07-18 00:13:21 +0800

    Add Activity Recognition permission and Play Services location dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
